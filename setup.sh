3f70d4f21eeab35186526d7ca2c4f4e47c6dd4ca
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-12-22 06:53:59 -0500

    Edit the workflow

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.pymirror/.addons
 cp -R pymirror/data ~/.pymirror
 res=$(curl -s https://api.github.com/repos/gorhill/uBlock/releases/latest | grep "firefox.xpi")
 URL=$(echo "{$(echo $res)}" | jq --raw-output '.browser_download_url')
-curl -sL $URL -o ./.addons/ublock_latest.xpi
+curl -sL $URL -o ~/.pymirror/.addons/ublock_latest.xpi
